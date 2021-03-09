43f837d8185fdb5d88ec577722fb1f50138ab874
noko1024 <ynoko1024@gmail.com>
2021-03-10 03:46:49 +0900

    ADD Token generate

---
+++ app/controllers/server_controller.rb
@@ -0,0 +1,12 @@
+class ServerController < ApplicationController
+
+  def clientHello
+    roomToken = params[:roomToken]
+    clientName = params[:name]
+    
+  end
+
+  def clientRequest
+    
+  end
+end
