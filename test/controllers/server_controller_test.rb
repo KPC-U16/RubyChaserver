3cfde0f6e0ca43b78404a67ef8bffc1dcc8f562b
noko1024 <ynoko1024@gmail.com>
2021-03-10 15:09:50 +0900

    DELETE OLD Rails Data

--- test/controllers/server_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ServerControllerTest < ActionDispatch::IntegrationTest
-  test "should get top" do
-    get server_top_url
-    assert_response :success
-  end
-end
--- test/controllers/token_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class TokenControllerTest < ActionDispatch::IntegrationTest
-  test "should get get" do
-    get token_get_url
-    assert_response :success
-  end
-end
