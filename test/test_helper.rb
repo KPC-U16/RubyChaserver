a7c60dc2ccdca898a6b7585fff652d0b94490361
noko1024 <ynoko1024@gmail.com>
2021-03-11 06:54:35 +0900

    UPDATE yml

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,11 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
-require 'simplecov'
-SimpleCov.start 'rails'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
